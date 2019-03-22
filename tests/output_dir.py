6cbf7bc90466f2c14d130016cdbcf69358a60153
David Whittingham <david.whittingham@dnrm.qld.gov.au>
2019-03-22 15:08:01 +1000

    Updated test documentation

--- tests/output_dir.py
+++ tests/output_dir.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-"""This module contains shared tests for all models that implement the SDDraftBase class."""
+"""This module contains shared tests for all models that implement output directories."""
 
 # Python 2/3 compatibility
 # pylint: disable=wildcard-import,unused-wildcard-import,wrong-import-order,wrong-import-position
