a88977c36b34c2ebda00b20862676c23d214db47
David Whittingham <DavidWhittingham@users.noreply.github.com>
2019-05-16 21:20:31 +1000

    Updated pie_tasks.py to work cross platform

--- pie_tasks.py
+++ pie_tasks.py
@@ -1,4 +1,8 @@
+import os
 from pie import *
+
+# Get OS-specific partial path
+VENV_TEST = os.path.join("venv", "test")
 
 @task
 def build():
@@ -7,7 +11,7 @@ def build():
 
 @task
 def createVenvs():
-    venv(r"venvs\test").create("--system-site-packages")
+    venv(VENV_TEST)
 
 
 @task
@@ -18,16 +22,16 @@ def setup():
 
 @task
 def test():
-    with venv(r"venvs\test"):
+    with venv(VENV_TEST):
         cmd(r"python -m pytest -s --cov-report term --cov-report html --cov=agsconfig .\\tests")
 
 
 @task
 def updatePackages():
-    with venv(r"venvs\test"):
+    with venv(VENV_TEST):
         pip(r"install -U pip")
-        pip(r"install -U -r requirements.txt")
         pip(r"install -U -r requirements.test.txt")
+        pip(r"install -U -r requirements.txt")
 
 
 @task([OptionsParameter('version')])
