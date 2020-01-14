de84e6af6b27efef78bdb8b9de5932dbb7021877
David Whittingham <DavidWhittingham@users.noreply.github.com>
2020-01-15 08:09:16 +1000

    Bumped version number for release

--- agsconfig/_version.py
+++ agsconfig/_version.py
@@ -1,3 +1,3 @@
-__version__ = "0.1.19"
+__version__ = "0.1.20"
 __author__ = "David Whittingham; Chris Blanchfield"
-__copyright__ = "Copyright (C) 2019 David Whittingham"
+__copyright__ = "Copyright (C) 2019-2020 David Whittingham"
