a97b385d0efdeec306f9076518e760b4c8c556bb
David Whittingham <DavidWhittingham@users.noreply.github.com>
2022-06-10 14:08:45 +1000

    Updated version info

--- agsconfig/_version.py
+++ agsconfig/_version.py
@@ -1,3 +1,3 @@
-__version__ = "0.2.4"
-__author__ = "David Whittingham; Chris Blanchfield"
+__version__ = "0.2.5"
+__author__ = "David Whittingham; Chris Blanchfield; Gary Bagnall"
 __copyright__ = "Copyright (C) 2019-2022 David Whittingham"
